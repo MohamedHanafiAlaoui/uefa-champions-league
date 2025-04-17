389c54c15d1bd906985ea8cfa759b06c4e158a0d
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-04-17 12:24:15 +0100

    ajouter page not page et matches

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,13 +2,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
+import Matches from '../pages/Matches';
+import NotFound from '../pages/NotFound';
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-
+        <Route path="/matches" element={<Matches />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
